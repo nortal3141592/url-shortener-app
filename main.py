1afe3327ddf9155c1827675c8e81ab440bf0fdd4
Palin Jena <jena.palin@gmail.com>
2026-05-24 11:19:49 +0530

    feat: Post /urls endpoint with short code generation

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from app.db.database import Base, engine
+from app.api.v1.endpoints.urls import router
+
+Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
+app.include_router(router, prefix="/api/v1")
 
 @app.get('/health')
-def home():
-    return {"status": "ok"}
+def check_health():
+    return {"message": "healthy"}
