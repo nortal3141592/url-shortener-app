ac348c2627e1825b7ec4b8309acf8598658cb267
Palin Jena <jena.palin@gmail.com>
2026-05-25 08:51:38 +0530

    feat: add frontend UI

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 from app.db.database import Base, engine
 from app.api.v1.endpoints.url_routers import api_router, redirect_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router, prefix="/api/v1")
 app.include_router(redirect_router)
 
 @app.get('/health')
 def check_health():
     return {"message": "healthy"}
+
+
